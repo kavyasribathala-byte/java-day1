b53a449cffc4ad828f052b5e99fa7167985f87ce
kavyasribathala-byte <kavyasribathala@gmail.com>
2026-01-31 14:31:46 +0530

    Add files via upload

---
+++ demo1.java
@@ -0,0 +1,9 @@
+public class demo1
+{
+    public static void main (String[] args)
+    {
+        System.out.println("welcome to java");
+        System.out.printf("welcome to javaclass");
+        System.out.println("welcome to javaclub");
+    }
+}
